b3c41a7d9480d7917bf55fd15970e0dde2be65b2
Stsiapan Tsikhanau <stsiapan.tsikhanau@gmail.com>
2023-06-17 14:17:03 +0200

    Changed string and properties

--- Entities/Product.cs
+++ Entities/Product.cs
@@ -7,12 +7,12 @@ public class Product
     public string Description { get; set; }
     public string MetaData { get; set; }
     
-    public Brand Brand { get; set; }
+    public Brand? Brand { get; set; }
     public int BrandId { get; set; }
-    public Color PrimaryColor { get; set; }
+    public Color? PrimaryColor { get; set; }
     public int PrimaryColorId { get; set; }
-    public Category Category { get; set; }
+    public Category? Category { get; set; }
     public int CategoryId { get; set; }
-    public ICollection<ProductVariation> Variations { get; set; }
-    public ICollection<Review> Reviews { get; set; }
+    public ICollection<ProductVariation>? Variations { get; set; }
+    public ICollection<Review>? Reviews { get; set; }
 }
